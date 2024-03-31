e27c4471ea1bb1ac718daa8c775c58d5c10329bf
Code@Ahtesham <muhammadahtesham43@gmail.com>
2024-03-31 21:01:53 +0500

    CLI Number Guessing Game.

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 5);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter a number between 0 to 5:"
+    }
+]);
+if (answer.userGuessedNumber === randomNumber)
+    (console.log("Congratulations! You've entered the correct number."));
+else
+    (console.log("Sorry! You've entered the incorrect number."));
